21e2b1446c5343022b7a9eaa4b36d1da0d774165
Daniel Goncalves <dantecbrasil@gmail.com>
2026-07-31 21:14:42 -0300

    Abrir lista salva implementado

---
+++ lib/screens/detalhe_lista_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DetalheListaScreen extends StatelessWidget {
+  final Map<String, dynamic> lista;
+
+  const DetalheListaScreen({
+    super.key,
+    required this.lista,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final produtos = lista['produtos'];
+
+    final formatoMoeda = NumberFormat.currency(
+      locale: 'pt_BR',
+      symbol: 'R\$ ',
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(lista['nomeLista']),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.start,
+          children: [
+            Text(
+              lista['nomeLista'],
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: produtos.length,
+                itemBuilder: (context, index) {
+                  final produto =
+                      produtos[index];
+
+                  return Card(
+                    child: ListTile(
+                      leading:
+                          const Icon(Icons.shopping_cart),
+
+                      title: Text(
+                        produto['nome'],
+                      ),
+
+                      subtitle: Text(
+                        '${produto['quantidade']} x ${formatoMoeda.format(produto['preco'])}',
+                      ),
+
+                      trailing: Text(
+                        formatoMoeda.format(
+                          produto['subtotal'],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            Text(
+              'Total: ${formatoMoeda.format(lista['total'])}',
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/listas_screen.dart
+++ lib/screens/listas_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../data/listas_repository.dart';
+import 'detalhe_lista_screen.dart';
 
 class ListasScreen extends StatelessWidget {
   const ListasScreen({super.key});
@@ -35,6 +36,16 @@ class ListasScreen extends StatelessWidget {
                 return Card(
                   margin: const EdgeInsets.all(8),
                   child: ListTile(
+                    onTap: () {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => DetalheListaScreen(
+        lista: lista,
+      ),
+    ),
+  );
+},
                     leading: const Icon(
                       Icons.list_alt,
                     ),
